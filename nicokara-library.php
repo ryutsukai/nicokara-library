67661d0dc44efc99feef17589b75095dda91a7c1
YuichiFurutani <hackadon@botchsoft.com>
2017-05-13 00:17:31 +0900

    Update nicokara-library.php
    動画情報の取得を追加。

--- nicokara-library.php
+++ nicokara-library.php
@@ -1,4 +1,5 @@
 <?php
+    date_default_timezone_set('Asia/Tokyo');
     class nicokara{
         public $search_option = array();
         public $search_information = array();
@@ -13,11 +14,34 @@
             $this->search_information = $data[0][0]->information[0];
             $this->search_data = $data[0][1]->search;
         }
+        
         function watch($smid){
             $nicoAPI = simplexml_load_file("http://ext.nicovideo.jp/api/getthumbinfo/{$smid}");
             $this->watch_data['smid'] = $smid;
             $this->watch_data['title'] = $nicoAPI->thumb->title;
-            $this->watch_data['description'] = $nicoAPI->thumb->description;
+            $this->watch_data['description1'] = $nicoAPI->thumb->description;
+            $nicohtml = file_get_contents("http://www.nicovideo.jp/watch/".$smid);
+            $nicohtml = str_replace("http://www.nicovideo.jp/", "http://www.nicokara.net/", $nicohtml);
+            preg_match_all("/description\">(.*)<\/p>/",$nicohtml,$match3);
+            $desc = $match3[1][0];
+            $this->watch_data['description2'] = $desc;
             $this->watch_data['thumbnail_url'] = $nicoAPI->thumb->thumbnail_url;
+            $this->watch_data['first_retrieve'] = date('Y/m/d H:i', strtotime($nicoAPI->thumb->first_retrieve));
+            $this->watch_data['length'] = $nicoAPI->thumb->length;
+            $this->watch_data['movie_type'] = $nicoAPI->thumb->movie_type;
+            $this->watch_data['size_high'] = $nicoAPI->thumb->size_high;
+            $this->watch_data['size_low'] = $nicoAPI->thumb->size_low;
+            $this->watch_data['view_counter'] = $nicoAPI->thumb->view_counter;
+            $this->watch_data['comment_num'] = $nicoAPI->thumb->comment_num;
+            $this->watch_data['mylist_counter'] = $nicoAPI->thumb->mylist_counter;
+            $this->watch_data['last_res_body'] = $nicoAPI->thumb->last_res_body;
+            $this->watch_data['watch_url'] = $nicoAPI->thumb->watch_url;
+            $this->watch_data['thumb_type'] = $nicoAPI->thumb->thumb_type;
+            $this->watch_data['embeddable'] = $nicoAPI->thumb->embeddable;
+            $this->watch_data['no_live_play'] = $nicoAPI->thumb->no_live_play;
+            $this->watch_data['tags'] = $nicoAPI->thumb->tags;
+            $this->watch_data['user_id'] = $nicoAPI->thumb->user_id;
+            $this->watch_data['user_nickname'] = $nicoAPI->thumb->user_nickname;
+            $this->watch_data['user_icon_url'] = $nicoAPI->thumb->user_icon_url;
         }
     }
